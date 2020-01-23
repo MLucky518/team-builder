849326e7b1b339ea8eaf3e05290cf31b8c74b04c
MLucky518 <lucky32815@gmail.com>
2020-01-22 22:54:09 -0500

    mvp

---
+++ team-builder/src/data.js
@@ -0,0 +1,35 @@
+ export const teamMembers = [
+{
+    name:"john",
+    email:"john@gmail.com",
+    role: "team-lead"
+
+
+},
+{
+    name:"mike",
+    email:"mike@gmail.com",
+    role: "c# programmer"
+
+
+},
+{
+    name:"mitch",
+    email:"mitch@gmail.com",
+    role: "c++ programmer"
+
+
+},
+{
+    name:"chris",
+    email:"chris@gmail.com",
+    role: "art lead"
+
+
+},
+
+
+
+
+
+]
